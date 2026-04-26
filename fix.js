c64fab809569b9faee6a1fe4a7d1a0508abaa7c8
Sameer10062004 <jambulkarsameer@gmail.com>
2026-04-26 11:11:09 +0530

    Enhance text cleaning in fix.js
    Added additional replacements for zero-width characters and non-ASCII characters.

--- fix.js
+++ fix.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
 let c = fs.readFileSync('server.js', 'utf8');
-c = c.replace(/[^\x00-\x7F]/g, '');
+c = c.replace(/\u200B/g, '');
+c = c.replace(/\u200C/g, '');
+c = c.replace(/\u200D/g, '');
+c = c.replace(/\uFEFF/g, '');
+c = c.replace(/[^\x09\x0A\x0D\x20-\x7E]/g, '');
 fs.writeFileSync('server.js', c);
+console.log('Cleaned server.js');
