7397335c82629509df59a689b3f0046b097a8ad8
Sameer10062004 <jambulkarsameer@gmail.com>
2026-04-26 11:16:24 +0530

    Fix string quotes for BASE URL in server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const CLIENT_ID   = process.env.CLIENT_ID;
 const CLIENT_PIN  = process.env.CLIENT_PIN;
 const TOTP_SECRET = process.env.TOTP_SECRET;
 
-const BASE = ‘https://apiconnect.angelbroking.com’;
+const BASE = 'https://apiconnect.angelbroking.com';
 
 let session = { token: null, expiry: 0 };
 
